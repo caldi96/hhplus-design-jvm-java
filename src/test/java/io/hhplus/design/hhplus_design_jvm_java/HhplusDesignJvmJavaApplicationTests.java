92dfdb54dbd0e3dc3882a3972ed449465e51e4c7
parkboseung <endeveloper96@gmail.com>
2025-10-28 03:38:24 +0900

    첫번째 커밋

---
+++ src/test/java/io/hhplus/design/hhplus_design_jvm_java/HhplusDesignJvmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.hhplus.design.hhplus_design_jvm_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusDesignJvmJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
